9b778182ee813ae223d896e59c1b9291d01fc078
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-04-28 09:56:31 -0400

    unstatic

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -5,9 +5,9 @@
  */
 
 class Dog{
-    static String name;
-    static String breed;
-    static int weight;
+    String name;
+    String breed;
+    int weight;
 
     /**
      * Constructor of class Dog
@@ -25,36 +25,36 @@ class Dog{
      * Gets the name of the Dog
      * @return The name of the Dog
      */
-    static String getName(){return name;}
+    public String getName(){return name;}
     
     /**
      * Gets the breed of the Dog
      * @return The breed of the Dog
      */
-    static String getBreed(){return breed;}
+    public String getBreed(){return breed;}
     
     /**
      * Gets the weight of the Dog
      * @return The weight of the Dog
      */
-    static int getWeight(){return weight;}
+    public int getWeight(){return weight;}
     
 
     /**
      * Sets the name of the Dog
      * @param _name The name to be set
      */
-    public static void setName(String _name){name = _name;}
+    public void setName(String _name){name = _name;}
     
     /**
      * Sets the breed of the Dog
      * @param _breed The breed to be set
      */
-    public static void setBreed(String _breed){breed = _breed;}
+    public void setBreed(String _breed){breed = _breed;}
     
     /**
      * Sets the weight of the Dog
      * @param _weight The weight to be set
      */
-    public static void setWeight(int _weight){weight = _weight;}
+    public void setWeight(int _weight){weight = _weight;}
 }
